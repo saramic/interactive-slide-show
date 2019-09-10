0ab2aa0a18d91d88fb84216f92579d5f58525c50
Michael Milewski <saramic@gmail.com>
2019-09-10 19:27:00 +1000

    graphql fetch with polling

--- client/src/Slide.jsx
+++ client/src/Slide.jsx
@@ -1,21 +1,60 @@
 import React, { useState } from "react";
+import { ApolloClient } from "apollo-client";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
 
-const Display = ({ counts }) => (
-  <div className='flex items-stretch-'>
-    <div
-      className={`flex-1 ${counts[0] < counts[1] &&
-        "text-gray-500"} text-center px-4 py-2 m-2`}
-    >
-      {counts[0]}
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "http://localhost:4000/graphql"
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+const GET_VOTES = gql`
+  query getVotes {
+    getVotes {
+      id
+      option
+    }
+  }
+`;
+client
+  .query({
+    query: GET_VOTES
+  })
+  .then(result => console.log(result));
+
+const Display = ({ counts }) => {
+  const { data, loading, error, fetchMore } = useQuery(GET_VOTES, {
+    pollInterval: 1000
+  });
+  if (loading) return "loading...";
+  if (error) return "error...";
+
+  return (
+    <div className='flex items-stretch-'>
+      {JSON.stringify(data)}
+      {JSON.stringify(data.getVotes.length)}
+      <div
+        className={`flex-1 ${counts[0] < counts[1] &&
+          "text-gray-500"} text-center px-4 py-2 m-2`}
+      >
+        {counts[0]}
+      </div>
+      <div
+        className={`flex-1 ${counts[0] > counts[1] &&
+          "text-gray-500"} text-center px-4 py-2 m-2`}
+      >
+        {counts[1]}
+      </div>
     </div>
-    <div
-      className={`flex-1 ${counts[0] > counts[1] &&
-        "text-gray-500"} text-center px-4 py-2 m-2`}
-    >
-      {counts[1]}
-    </div>
-  </div>
-);
+  );
+};
 
 const Progress = ({ timeLeft }) => (
   <div className='flex items-stretch'>
@@ -63,13 +102,15 @@ const Slide = () => {
   };
 
   return (
-    <div className='text-gray-100'>
-      <Display counts={counts} />
-      <div className='w-full pin-b absolute bottom-0 left-0'>
-        <Progress timeLeft={timeLeft} />
-        <Choices updateCount={updateCount} />
+    <ApolloProvider client={client}>
+      <div className='text-gray-100'>
+        <Display counts={counts} />
+        <div className='w-full pin-b absolute bottom-0 left-0'>
+          <Progress timeLeft={timeLeft} />
+          <Choices updateCount={updateCount} />
+        </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
 };
 
